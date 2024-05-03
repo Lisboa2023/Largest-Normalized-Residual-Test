5d0c6818e942dbab3ff7c0f5aada9c42e811ebd3
Lisboa2023 <168318731+Lisboa2023@users.noreply.github.com>
2024-05-03 17:18:52 -0300

    Add files via upload

--- teste.cpp
+++ teste.cpp
@@ -1,58 +1,99 @@
 //Largest resiudal normalized
-//
 
 #include<iostream>
+#include<cmath>
+#include<iomanip>
 
 using namespace std;
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::setw;
 
-void calculateNormalizedResidue();
-void detectError(int, float[], float &, int &);
-void delError(int, int, float[], float*, float, int p);
-void print(int, float[]);
+const int tam = 5; 
+
+void calculateResidualArray(int, float *, float *, float *);
+void calculateNormalizedResidual(float *, float *, double [][tam], int);
+void findLargestResidual(int, float *, float &, int &);
+void deletError(int, float *, float, int);
+void print(int, float *);
+void print(float);
+void print(int, double [][tam]);
 
 int main(){
     const int size = 5;
-    const float threshold = 3.0;
-    int count = 0;
-    float array[] = {4.2, 4.1, 2.3, 2.3, 1.1};
-    float largestResidual;
-    int p;
 
-    detectError(size, array, largestResidual, p);
-    
-    int tam = size - count;
-    float *ptr = new float[tam];
-    
-    delError(size, threshold, array, ptr, largestResidual, p);
-    print(tam,ptr);
+    float measurementArray[] = {1.0285, 1.0121, 0.9893, 0.4796, 0.3891};
+    float estimatedMeasurements[] = {0.9999, 0.9886, 0.9833, 0.4856, 0.3821};
+    double covarianceMatrix[][size] = {{0.00004637, 0,0,0,0},
+                                       {0,0.00003285,0,0,0},
+                                       {0,0, 0.000006805,0,0},
+                                       {0,0,0, 0.000006805,0},
+                                       {0,0,0,0, 0.0000405}};
+
+    // float residualArray[] = {0.0286, 0.0235, 0.006, -0.006, 0.007};
+    float residualArray[size];
+    float normalizedArray[size];
+
+    calculateResidualArray(size, residualArray, measurementArray, estimatedMeasurements);
+    calculateNormalizedResidual(normalizedArray, residualArray, covarianceMatrix, size);
+    print(size, normalizedArray);
+
+    float largestResidual;
+    int position;
+    const float threshold = 3.0;
+
+    for(int i=0; i<size; i++){
+        findLargestResidual(size, normalizedArray, largestResidual, position); 
+        deletError(threshold, normalizedArray, largestResidual, position);
+    }
+    print(size,normalizedArray);
 
     return 0;
 }
 
-void calculateNormalizedResidue(float rn[], int size){
+void calculateResidualArray(int size, float *r, float *measurement, float *mEstimated){
     for(int i = 0; i < size; i++){
-        rn[i] = abs(r[i])/sqrt(cm[i][i]);
+        *(r+i) = *(measurement+i) - *(mEstimated+i);
     }
 }
 
-void detectError(int size, float array[], float &temp, int &position){
-    temp = array[0];
+void calculateNormalizedResidual(float *rn, float *r, double cm[][tam], int size){
     for(int i = 0; i < size; i++){
-        if(array[i] >= temp){
-            temp = array[i];
-            position = i;
+        *(rn+i) = abs(*(r+i))/sqrt(cm[i][i]);
+    }
+}
+
+void findLargestResidual(int size, float *array, float &temp, int &pos){
+    temp = *array;
+    for(int i = 0; i < size; i++){
+        if(*(array+i) >= temp){
+            temp = *(array + i);
+            pos = i;
         }
     }
 }
 
-void delError(int size, int threshold, float array[], float *ptr, float lg, int p){
-
-        }
-    }
+void deletError(int threshold, float *array, float lg, int p){
+    if(lg > threshold){
+        *(array+p) = 0;
+    }  
 }
 
-void print(int tam, float ptr[]){
-    for(int i = 0; i<tam; i++){
-        cout << ptr[i] << endl;
+void print(int size, float *ptr){
+    for(int i = 0; i<size; i++){
+        cout << setw(10) << setprecision(2) << *(ptr+i) ;
+    }
+
+    cout << endl;
+}
+
+void print(int size, double ptr[][tam]){
+    for(int i = 0; i<size; i++){
+        for(int j=0; j<size; j++){
+            cout << setw(10) << setprecision(2) << ptr[i][j] << "\t";
+        }
+
+        cout << endl;
     }
 }
